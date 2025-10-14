9cbf43794a91d09a2df791336cbb81266ec33792
julien.soum <julienl.soum@artfx.fr>
2025-10-14 09:31:24 +0200

    Clear window.py

--- IrisPipeline/ui/window.py
+++ IrisPipeline/ui/window.py
@@ -10,20 +10,6 @@ from qtpy import QtCore, QtWidgets, QtGui
 
 ui_path = os.path.join(os.path.dirname(__file__), "qt/browser.ui")
 
-class WebWindow(QtWidgets.QMainWindow):
-    def __init__(self, url: str, title: str = "Web", size=(1024, 750), parent=None):
-        super().__init__(parent)
-        self.setAttribute(Qt.WA_DeleteOnClose, True)
-        self.setWindowTitle(title)
-        self.resize(*size)
-
-        self.view = QWebEngineView(self)
-        self.setCentralWidget(self.view)
-        self.view.load(QUrl.fromUserInput(url))
-
-    def load_url(self, url: str):
-        self.view.load(QUrl.fromUserInput(url))
-
 class Browser(QtWidgets.QMainWindow):
 
     cb = QtWidgets.QApplication.clipboard()
@@ -33,21 +19,6 @@ class Browser(QtWidgets.QMainWindow):
         from qtpy.uic import loadUi
         loadUi(ui_path, self)
         self.setWindowTitle("Browser")
-
-        self._windows  = []
-        self.PB_help.clicked.connect(self.on_help_clicked)
-
-    @Slot()
-    def on_help_clicked(self):
-        self.open_url("https://www.youtube.com/watch?v=xvFZjo5PgG0", title="Help")
-
-    def open_url(self, url: str, title: str = "Web"):
-        win = WebWindow(url=url, title=title, parent=None)
-        win.show()
-
-        self._windows.append(win)
-
-        win.destroyed.connect(lambda *_: self._windows.remove(win) if win in self._windows else None)
 
 app = QtWidgets.QApplication(sys.argv)
 b = Browser()
